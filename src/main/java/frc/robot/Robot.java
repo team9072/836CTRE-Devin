c5e4cc2cd4d5a1a08358d8771c684e40399951ca
TigerBots <118773412+TigerBotsCSP@users.noreply.github.com>
2024-06-13 17:40:29 -0400

    Remove flywheel tuning code and enable compressor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -66,9 +66,7 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void testPeriodic() {
-    m_robotContainer.testFlywheel();
-  }
+  public void testPeriodic() {}
 
   @Override
   public void testExit() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,7 +13,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -69,17 +68,11 @@ public class RobotContainer {
   }
 
   public RobotContainer() {
+    m_Compressor.enableDigital();
     configureBindings();
-    m_Compressor.disable();
-    SmartDashboard.putNumber("Expected Flywheel Velocity", 0);
   }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
-  }
-
-  public void testFlywheel() {
-    m_flywheel.setVelocity(SmartDashboard.getNumber("Expected Flywheel Velocity", 0));
-    SmartDashboard.putNumber("Flywheel Velocity", m_flywheel.getVelocity());
   }
 }
